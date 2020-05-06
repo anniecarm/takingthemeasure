45da4c24bda26ffc1de8ff7a293b48e18d29810a
Annie Carmichael <42788203+anniecarm@users.noreply.github.com>
2020-05-06 16:18:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,288 @@
+$(document).ready(function () {
+	$(".c1").click(function() {
+		$(".chart").removeClass("hidden");
+			$(".c2").attr("style", "background-color:white");
+			$(".c3").attr("style", "background-color:white");
+			$(".c4").attr("style", "background-color:white");
+			$(".c5").attr("style", "background-color:white");
+			$(".c6").attr("style", "background-color:white");
+			$(".c7").attr("style", "background-color:white");
+			$(".c8").attr("style", "background-color:white");
+			$(".c9").attr("style", "background-color:white");
+			$(".c10").attr("style", "background-color:white");
+			$(".next").removeClass("hidden");
+			$(".label").removeClass("hidden");
+			$(".labelbig").removeClass("hidden");
+			$(".numberbig").removeClass("hidden");
+			$(".number").removeClass("hidden");
+			$(".anxbig").removeClass("hidden");
+			$(".nobig").removeClass("hidden");
+			$(".youbig").removeClass("hidden");
+			$(".anx").removeClass("hidden");
+			$(".no").removeClass("hidden");
+			$(".you").removeClass("hidden");
+
+		});
+
+	});
+
+
+$(document).ready(function () {
+	$(".c2").click(function() {
+		$(".chart").removeClass("hidden");
+			$(".c1").attr("style", "background-color:white");
+			$(".c3").attr("style", "background-color:white");
+			$(".c4").attr("style", "background-color:white");
+			$(".c5").attr("style", "background-color:white");
+			$(".c6").attr("style", "background-color:white");
+			$(".c7").attr("style", "background-color:white");
+			$(".c8").attr("style", "background-color:white");
+			$(".c9").attr("style", "background-color:white");
+			$(".c10").attr("style", "background-color:white");
+			$(".next").removeClass("hidden");
+			$(".label").removeClass("hidden");
+			$(".labelbig").removeClass("hidden");
+			$(".numberbig").removeClass("hidden");
+			$(".number").removeClass("hidden");
+			$(".anxbig").removeClass("hidden");
+			$(".nobig").removeClass("hidden");
+			$(".youbig").removeClass("hidden");
+			$(".anx").removeClass("hidden");
+			$(".no").removeClass("hidden");
+			$(".you").removeClass("hidden");
+
+		});
+
+	});
+
+$(document).ready(function () {
+	$(".c3").click(function() {
+		$(".chart").removeClass("hidden");
+		$(".c1").attr("style", "background-color:white");
+			$(".c2").attr("style", "background-color:white");
+			$(".c4").attr("style", "background-color:white");
+			$(".c5").attr("style", "background-color:white");
+			$(".c6").attr("style", "background-color:white");
+			$(".c7").attr("style", "background-color:white");
+			$(".c8").attr("style", "background-color:white");
+			$(".c9").attr("style", "background-color:white");
+			$(".c10").attr("style", "background-color:white");
+			$(".next").removeClass("hidden");
+			$(".label").removeClass("hidden");
+			$(".labelbig").removeClass("hidden");
+			$(".numberbig").removeClass("hidden");
+			$(".number").removeClass("hidden");
+			$(".anxbig").removeClass("hidden");
+			$(".nobig").removeClass("hidden");
+			$(".youbig").removeClass("hidden");
+			$(".anx").removeClass("hidden");
+			$(".no").removeClass("hidden");
+			$(".you").removeClass("hidden");
+
+		});
+
+	});
+
+$(document).ready(function () {
+	$(".c4").click(function() {
+		$(".chart").removeClass("hidden");
+		$(".c1").attr("style", "background-color:white");
+			$(".c2").attr("style", "background-color:white");
+			$(".c3").attr("style", "background-color:white");
+			$(".c5").attr("style", "background-color:white");
+			$(".c6").attr("style", "background-color:white");
+			$(".c7").attr("style", "background-color:white");
+			$(".c8").attr("style", "background-color:white");
+			$(".c9").attr("style", "background-color:white");
+			$(".c10").attr("style", "background-color:white");
+			$(".next").removeClass("hidden");
+			$(".label").removeClass("hidden");
+			$(".labelbig").removeClass("hidden");
+			$(".numberbig").removeClass("hidden");
+			$(".number").removeClass("hidden");
+			$(".anxbig").removeClass("hidden");
+			$(".nobig").removeClass("hidden");
+			$(".youbig").removeClass("hidden");
+			$(".anx").removeClass("hidden");
+			$(".no").removeClass("hidden");
+			$(".you").removeClass("hidden");
+
+		});
+
+	});
+
+$(document).ready(function () {
+	$(".c5").click(function() {
+		$(".chart").removeClass("hidden");
+		$(".c1").attr("style", "background-color:white");
+			$(".c2").attr("style", "background-color:white");
+			$(".c3").attr("style", "background-color:white");
+			$(".c4").attr("style", "background-color:white");
+			$(".c6").attr("style", "background-color:white");
+			$(".c7").attr("style", "background-color:white");
+			$(".c8").attr("style", "background-color:white");
+			$(".c9").attr("style", "background-color:white");
+			$(".c10").attr("style", "background-color:white");
+			$(".next").removeClass("hidden");
+			$(".label").removeClass("hidden");
+			$(".labelbig").removeClass("hidden");
+			$(".numberbig").removeClass("hidden");
+			$(".number").removeClass("hidden");
+			$(".anxbig").removeClass("hidden");
+			$(".nobig").removeClass("hidden");
+			$(".youbig").removeClass("hidden");
+			$(".anx").removeClass("hidden");
+			$(".no").removeClass("hidden");
+			$(".you").removeClass("hidden");
+		});
+
+	});
+
+$(document).ready(function () {
+	$(".c6").click(function() {
+		$(".chart").removeClass("hidden");
+		$(".c1").attr("style", "background-color:white");
+			$(".c2").attr("style", "background-color:white");
+			$(".c3").attr("style", "background-color:white");
+			$(".c4").attr("style", "background-color:white");
+			$(".c5").attr("style", "background-color:white");
+			$(".c7").attr("style", "background-color:white");
+			$(".c8").attr("style", "background-color:white");
+			$(".c9").attr("style", "background-color:white");
+			$(".c10").attr("style", "background-color:white");
+			$(".next").removeClass("hidden");
+			$(".label").removeClass("hidden");
+			$(".labelbig").removeClass("hidden");
+			$(".numberbig").removeClass("hidden");
+			$(".number").removeClass("hidden");
+			$(".anxbig").removeClass("hidden");
+			$(".nobig").removeClass("hidden");
+			$(".youbig").removeClass("hidden");
+			$(".anx").removeClass("hidden");
+			$(".no").removeClass("hidden");
+			$(".you").removeClass("hidden");
+		});
+
+	});
+
+$(document).ready(function () {
+	$(".c7").click(function() {
+		$(".chart").removeClass("hidden");
+		$(".c1").attr("style", "background-color:white");
+			$(".c2").attr("style", "background-color:white");
+			$(".c3").attr("style", "background-color:white");
+			$(".c4").attr("style", "background-color:white");
+			$(".c5").attr("style", "background-color:white");
+			$(".c6").attr("style", "background-color:white");
+			$(".c8").attr("style", "background-color:white");
+			$(".c9").attr("style", "background-color:white");
+			$(".c10").attr("style", "background-color:white");
+			$(".next").removeClass("hidden");
+			$(".label").removeClass("hidden");
+			$(".labelbig").removeClass("hidden");
+			$(".numberbig").removeClass("hidden");
+			$(".number").removeClass("hidden");
+			$(".anxbig").removeClass("hidden");
+			$(".nobig").removeClass("hidden");
+			$(".youbig").removeClass("hidden");
+			$(".anx").removeClass("hidden");
+			$(".no").removeClass("hidden");
+			$(".you").removeClass("hidden");
+
+		});
+
+	});
+
+$(document).ready(function () {
+	$(".c8").click(function() {
+		$(".chart").removeClass("hidden");
+		$(".c1").attr("style", "background-color:white");
+			$(".c2").attr("style", "background-color:white");
+			$(".c3").attr("style", "background-color:white");
+			$(".c4").attr("style", "background-color:white");
+			$(".c5").attr("style", "background-color:white");
+			$(".c6").attr("style", "background-color:white");
+			$(".c7").attr("style", "background-color:white");
+			$(".c9").attr("style", "background-color:white");
+			$(".c10").attr("style", "background-color:white");
+			$(".next").removeClass("hidden");
+			$(".label").removeClass("hidden");
+			$(".labelbig").removeClass("hidden");
+			$(".numberbig").removeClass("hidden");
+			$(".number").removeClass("hidden");
+			$(".anxbig").removeClass("hidden");
+			$(".nobig").removeClass("hidden");
+			$(".youbig").removeClass("hidden");
+			$(".anx").removeClass("hidden");
+			$(".no").removeClass("hidden");
+			$(".you").removeClass("hidden");
+
+		});
+
+	});
+
+$(document).ready(function () {
+	$(".c9").click(function() {
+		$(".chart").removeClass("hidden");
+		$(".c1").attr("style", "background-color:white");
+			$(".c2").attr("style", "background-color:white");
+			$(".c3").attr("style", "background-color:white");
+			$(".c4").attr("style", "background-color:white");
+			$(".c5").attr("style", "background-color:white");
+			$(".c6").attr("style", "background-color:white");
+			$(".c7").attr("style", "background-color:white");
+			$(".c8").attr("style", "background-color:white");
+			$(".c10").attr("style", "background-color:white");
+			$(".next").removeClass("hidden");
+			$(".label").removeClass("hidden");
+			$(".labelbig").removeClass("hidden");
+			$(".numberbig").removeClass("hidden");
+			$(".number").removeClass("hidden");
+			$(".anxbig").removeClass("hidden");
+			$(".nobig").removeClass("hidden");
+			$(".youbig").removeClass("hidden");
+			$(".anx").removeClass("hidden");
+			$(".no").removeClass("hidden");
+			$(".you").removeClass("hidden");
+
+		});
+
+	});
+
+$(document).ready(function () {
+	$(".c10").click(function() {
+		$(".chart").removeClass("hidden");
+		$(".c1").attr("style", "background-color:white");
+			$(".c2").attr("style", "background-color:white");
+			$(".c3").attr("style", "background-color:white");
+			$(".c4").attr("style", "background-color:white");
+			$(".c5").attr("style", "background-color:white");
+			$(".c6").attr("style", "background-color:white");
+			$(".c7").attr("style", "background-color:white");
+			$(".c8").attr("style", "background-color:white");
+			$(".c9").attr("style", "background-color:white");
+			$(".next").removeClass("hidden");
+			$(".label").removeClass("hidden");
+			$(".labelbig").removeClass("hidden");
+			$(".numberbig").removeClass("hidden");
+			$(".number").removeClass("hidden");
+			$(".anxbig").removeClass("hidden");
+			$(".nobig").removeClass("hidden");
+			$(".youbig").removeClass("hidden");
+			$(".anx").removeClass("hidden");
+			$(".no").removeClass("hidden");
+			$(".you").removeClass("hidden");
+		});
+
+	});
+
+$(document).ready(function () {
+	$(".nextimage").mouseover(function(){
+		$(this).attr("src","images/arroworange.png");
+	});
+});
+
+
+
+
+
